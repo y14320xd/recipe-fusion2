68a3ad539517205c74711faa41bfbe0495cfdde4
y14320xd <ian02280228@gmail.com>
2026-05-27 18:15:58 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/recipe-fusion2/", // 🔥 修正白畫面的核心關鍵！路徑必須對應你的 GitHub 儲存庫名稱
-})

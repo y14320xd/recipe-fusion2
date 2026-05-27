c111612ef6885bb346389ad13e4a59b69b4b39fc
y14320xd <ian02280228@gmail.com>
2026-05-27 18:02:57 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/recipe-fusion2/', // 必須跟你的專案名稱一致
+  base: "/recipe-fusion2/", // 🔥 修正白畫面的核心關鍵！路徑必須對應你的 GitHub 儲存庫名稱
 })
